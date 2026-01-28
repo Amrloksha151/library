8e83e3b8baa658bd373038faa99c2a0b6b7297d2
Amr Loksha <amrloksha151@proton.me>
2026-01-28 14:44:28 +0200

    finishing load books function

--- script.js
+++ script.js
@@ -24,6 +24,40 @@ function addBookToLibrary(title, author, pages, read) {
 function loadBooks() {
     for (let i = 0; i < myLibrary.length; i++) {
         const BOOK = myLibrary[i];
-        const TABLE = document.getElementById("books")
+        const TABLE = document.getElementById("books");
+        TABLE.innerHTML += `<tr>
+        <th scope="row">
+            ${i + 1}
+        </th>
+        <td>
+            ${BOOK.title}
+        </td>
+        <td>
+            ${BOOK.author}
+        </td>
+        <td>
+            ${BOOK.pages}
+        </td>
+        <td>
+            ${BOOK.read ? "Yes" : "No"}
+        </td>
+        <td>
+            <button onclick="toggleRead(${BOOK.id})" class="btn btn-dark">Change</button>
+        </td>
+        <td>
+            <button onclick="removeBook(${BOOK.id})" class="btn btn-danger">Remove</button>
+        </td>
+        </tr>`; 
     }
 }
+
+function toggleRead(book_id) {
+
+}
+
+function removeBook(book_id) {
+
+}
+
+addBookToLibrary("Linux", "amr loksha", 1000, false);
+loadBooks();
