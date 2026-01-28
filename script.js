0976bcbc0ca968c375d702b0f0a5ed92b23ca687
Amr Loksha <amrloksha151@proton.me>
2026-01-28 15:28:44 +0200

    finishing current features and adding getBookById

--- script.js
+++ script.js
@@ -22,9 +22,10 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 function loadBooks() {
+    const TABLE = document.getElementById("books");
+    TABLE.innerHTML = '';
     for (let i = 0; i < myLibrary.length; i++) {
         const BOOK = myLibrary[i];
-        const TABLE = document.getElementById("books");
         TABLE.innerHTML += `<tr>
         <th scope="row">
             ${i + 1}
@@ -42,21 +43,34 @@ function loadBooks() {
             ${BOOK.read ? "Yes" : "No"}
         </td>
         <td>
-            <button onclick="toggleRead(${BOOK.id})" class="btn btn-dark">Change</button>
+            <button onclick="toggleRead('${BOOK.id}')" class="btn btn-dark">Change</button>
         </td>
         <td>
-            <button onclick="removeBook(${BOOK.id})" class="btn btn-danger">Remove</button>
+            <button onclick="removeBook('${BOOK.id}')" class="btn btn-danger">Remove</button>
         </td>
         </tr>`; 
     }
 }
 
-function toggleRead(book_id) {
+function getBookById(book_id) {
+    for (let i = 0; i < myLibrary.length; i++) {
+        const BOOK = myLibrary[i];
+        if (BOOK.id === book_id) {
+            return BOOK;
+        }
+    }
+}
 
+function toggleRead(book_id) {
+    const BOOK = getBookById(book_id);
+    BOOK.read = !BOOK.read;
+    loadBooks();
 }
 
 function removeBook(book_id) {
-
+    const BOOK = getBookById(book_id)
+    myLibrary.splice(myLibrary.indexOf(BOOK), 1);
+    loadBooks();
 }
 
 addBookToLibrary("Linux", "amr loksha", 1000, false);
