e1f01347812ee7c9e9300c286745d102c1356995
Amr Loksha <amrloksha151@proton.me>
2026-01-28 13:57:00 +0200

    adding Book constructor main components

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  if (!new.target) {
+    throw Error("This cannot be used outside object contexts. Please use it with the 'new' constructor.")
+  }
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  if (typeof pages !== 'number') {
+    throw Error("Number of pages should be of type 'number'.")
+  } else if (typeof read !== 'boolean') {
+    throw Error("Read property should be of type 'boolean'.")
+  }
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
